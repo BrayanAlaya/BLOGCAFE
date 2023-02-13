48a8c7fa8c8fec3d7ea67c2edbee089196b4becc
BrayanAlaya <brayan.alaya@hotmail.com>
2023-02-13 15:32:14 -0500

    Modificacion V2

--- js/script.js
+++ js/script.js
@@ -3,4 +3,16 @@ const heading = document.querySelector("#heading__texto");
 
 const head = document.getElementById("heading__texto");
 
+const newA = document.createElement("A");
+newA.href = "contacto.html";
+newA.textContent = "Nuevo";
+newA.classList.add("navegacion__enlace");
+
+document.querySelectorAll(".navegacion")[0].appendChild(newA);
+document.querySelectorAll(".navegacion")[1].appendChild(newA);
+
+console.log(newA);
+
+
+
 
