9f347c4e187e2771a09bbac7a039c2e9887bd721
BrayanAlaya <brayan.alaya@hotmail.com>
2023-02-13 16:42:59 -0500

    nueva actualizacion v2

--- js/script.js
+++ js/script.js
@@ -3,15 +3,35 @@ const heading = document.querySelector("#heading__texto");
 
 const head = document.getElementById("heading__texto");
 
+// window.addEventListener("load", () =>{
+
+//     console.log(botonFormulario);
+// })
 const newA = document.createElement("A");
 newA.href = "contacto.html";
 newA.textContent = "Nuevo";
 newA.classList.add("navegacion__enlace");
 
 document.querySelectorAll(".navegacion")[0].appendChild(newA);
-document.querySelectorAll(".navegacion")[1].appendChild(newA);
 
-console.log(newA);
+const botonFormulario = document.querySelector(".boton");
+botonFormulario.addEventListener("click", e => {
+    console.log("hola")
+    e.preventDefault();
+})
+
+// const botonNuevo = document.createElement("input")
+// botonNuevo.type = "submit";
+// botonNuevo.value = "Nuevo boton"
+// botonNuevo.classList.add("boton");
+// botonNuevo.classList.add("boton--primario");
+// document.querySelectorAll(".imagen__bg")[0].appendChild(botonNuevo)
+
+// botonNuevo.addEventListener("click", ()=>{
+//     console.log("hola")
+// })
+
+
 
 
 
