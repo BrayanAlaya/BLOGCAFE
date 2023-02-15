c49e0aa4ee4127a22fbfa9518048e4b062dbe50e
BrayanAlaya <brayan.alaya@hotmail.com>
2023-02-15 17:27:27 -0500

    Validador de formularios

--- js/script.js
+++ js/script.js
@@ -14,12 +14,6 @@ newA.classList.add("navegacion__enlace");
 
 document.querySelectorAll(".navegacion")[0].appendChild(newA);
 
-const botonFormulario = document.querySelector(".boton");
-botonFormulario.addEventListener("click", e => {
-    console.log("hola")
-    e.preventDefault();
-})
-
 // const botonNuevo = document.createElement("input")
 // botonNuevo.type = "submit";
 // botonNuevo.value = "Nuevo boton"
@@ -31,8 +25,79 @@ botonFormulario.addEventListener("click", e => {
 //     console.log("hola")
 // })
 
+// datos de formulario
 
+const formulario = {
+    nombre : "",
+    email: "",
+    mensaje: ""
+}
 
+let contadorIntentos = 0;
+const errorNombre = document.createElement("p");
+const errorEmail = document.createElement("p");
+const errorMensaje = document.createElement("p");
+const nombre = document.querySelector("#nombre")
+nombre.addEventListener("input",agregarDatos)
+const email = document.querySelector("#email")
+email.addEventListener("input", agregarDatos)
+const mensaje = document.querySelector("#mensaje")
+mensaje.addEventListener("input", agregarDatos)
 
+function agregarDatos(e){
+    formulario [String(e.target.id)] = String(e.target.value);
+    const {nombre,email,mensaje} = formulario;
+    if (nombre !== "" || email !== "" || mensaje !== "") {
+        if (nombre != "") {
+            errorNombre.remove();
+        };
+        if (email != "") {
+            errorEmail.remove();
+        };
+        if (mensaje != "") {
+            errorMensaje.remove();
+        };
+    }
+    if (contadorIntentos != 0) {
+        if (nombre === "" || email === "" || mensaje === "") {
+            if (nombre === "") {
+                mostrarError("Nombre obligatorio",formSend,errorNombre);
+            }
+            if (email === "") {
+                mostrarError("Ingrese el email",formSend,errorEmail);
+            }
+            if (mensaje === "") {
+                mostrarError("Ingrese el mensaje",formSend,errorMensaje);
+            }
+        }
+    }
+}
 
+//submit para formulario
+
+const formSend = document.querySelector(".Contacto__formulario")
+formSend.addEventListener("submit",(evento)=>{
+    evento.preventDefault();
+    const {nombre,email,mensaje} = formulario;
+    if (nombre === "" || email === "" || mensaje === "") {
+        contadorIntentos = 1;
+        if (nombre === "") {
+            mostrarError("Nombre obligatorio",formSend,errorNombre);
+        }
+        if (email === "") {
+            mostrarError("Ingrese el email",formSend,errorEmail);
+        }
+        if (mensaje === "") {
+            mostrarError("Ingrese el mensaje",formSend,errorMensaje);
+        }
+        return;
+    }
+    errorNombre.textContent = "Genial pasaste"
+    formSend.appendChild(errorNombre)
+})
+function mostrarError(mensaje,agregar,error){
+    error.textContent = mensaje;
+    error.classList.add("error__mensaje");
+    agregar.appendChild(error);
+}
 
